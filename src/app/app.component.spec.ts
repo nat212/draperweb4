f734be2805302ea4366b0e06ec3edbafd38bb6b0
Natasha Draper <natasha.draper@byteorbit.com>
2022-10-04 10:43:48 +0200

    DW stuff

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'draperweb4'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('draperweb4');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'draperweb4 app is running!',
-    );
-  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const colours = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
+    colors: {
+      primary: colours.indigo,
+      accent: colours.cyan,
+      slate: colours.slate,
+      neutral: colours.zinc,
+    },
     extend: {},
   },
   plugins: [],
